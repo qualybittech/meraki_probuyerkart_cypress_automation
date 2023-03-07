1b2905511443c80e50c33f91a82ee74410294435
Ankit Singh <ankitsingh16121998@gmail.com>
2023-03-07 08:35:03 +0530

    whatsapp Field validation

--- cypress/e2e/fieldValidationWhatsApp.cy.js
+++ cypress/e2e/fieldValidationWhatsApp.cy.js
@@ -20,20 +20,5 @@ describe('Enable WhatsApp floating button on Meraki home page', () => {
         AdminPage.navigateApplication();
         AdminPage.clearFieldValue();
         AdminPage.fieldValidation();
-        // // Navigating to Meraki homepage for verification
-        // Home.openUrl(myTestData.url);
-        // Home.checkWhatsAppVisiblity();
     });
-
-    // it('Disable the whatsapp floating button', () => {
-    //     //Admin Login Page
-    //     Home.openUrl(myTestData.adminUrl);
-    //     AdminPage.login(myTestData.adminId, myTestData.adminPassword);
-    //     // Admin Home Page
-    //     AdminPage.getMenuApp();
-    //     //Disabling the whatsapp floating button
-    //     AdminPage.navigateApplication();
-    //     AdminPage.whatsAppSettings(myTestData.header_title, myTestData.header_message, 'Disable');
-    // });
-
 });
